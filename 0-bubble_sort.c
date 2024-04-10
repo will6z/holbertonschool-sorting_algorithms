b862a9c4f6262ab6673f6d5fd8f9dfb5b1c62f21
William <will@penguin.com>
2024-04-10 10:35:31 -0400

    first commit of the project

---
+++ 0-bubble_sort.c
@@ -0,0 +1,37 @@
+#include "sort.h"
+#include <stdlib.h>
+
+/**
+ * bubble_sort - sorts array of ints
+ * @array: input
+ * @size: input
+ * @swap: input
+ * Return: 0
+ */
+void bubble_sort(int *array, size_t size)
+{
+	size_t i, j;
+	int t;
+	int swap;
+
+	if (array == NULL || size < 2)
+		return;
+
+	for (i = 0; i < size - 1; i++)
+	{
+		swap = 0;
+		for (j = 0; j < size - i - 1; j++)
+		{
+			if (array[j] > array[j + 1])
+			{
+				t = array[j];
+				array[j] = array[j + 1];
+				array[j + 1] = t;
+				print_array(array, size);
+				swap = 1;
+			}
+		}
+		if (swap == 0)
+			break;
+	}
+}
